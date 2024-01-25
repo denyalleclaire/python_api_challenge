a07b87f6844a7634690377db775a71cd6e0b74de
Denyal <denyalleclaire@icloud.com>
2024-01-25 14:24:25 -0700

    part 1 upload

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_key = 'eafea4372d72461a2cb19159ae4600a4'
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = 'c09ff26be63551120f00c297c441657c'
